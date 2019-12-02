8d0ae5df71d3137b9359988a06a31e1896363493
Nate Whitehead <n.whitehead@elsevier.com>
2019-12-02 10:56:21 -0500

    cleaned stuff up and added TODOs

--- model_config.py
+++ model_config.py
@@ -1,46 +1,19 @@
 from pykg2vec.config.config import TransEConfig
-from pykg2vec.config.config import TransEConfig, NTNConfig
-from pykg2vec.core.TransE import TransE
 from pykg2vec.utils.bayesian_optimizer import BaysOptimizer
-from pykg2vec.config.hyperparams import KGETuneArgParser
 
 
 def transe_config(args, bayes_opt: bool = False):
-    '''
-    golden hyperparams
-    self.learning_rate = 0.01
-    self.L1_flag = True
-    self.hidden_size = 50
-    self.batch_size = 512
-    self.epochs = 500
-    self.margin = 1.0
-    self.data = 'Freebase15k'
-    self.optimizer = 'adam'
-    self.sampling = "uniform"
-    '''
+    args.learning_rate = 0.01
+    args.epochs = 200
+    args.margin = 2.0
+    args.batch_training = 256
+
     if bayes_opt:
         # Not sure what to do with this yet...
+        # Currently using -ghp arg - only implemented for Freebase15k!
         golden = bayesian_optimization(args=args)
 
     return TransEConfig(args=args)
-
-
-def ntn_config(args):
-    '''
-    Base configuration:
-    self.learning_rate = 0.01
-    self.L1_flag = True
-    self.ent_hidden_size = 64
-    self.rel_hidden_size = 32
-    self.batch_size = 128
-    self.epochs = 2
-    self.margin = 1.0
-    self.data = 'Freebase15k'
-    self.optimizer = 'adam'
-    self.sampling = "uniform"
-    '''
-    return NTNConfig(args=args)
-
 
 def bayesian_optimization(args):
     bayes_opt = BaysOptimizer(args=args)
--- rdf_recommender.py
+++ rdf_recommender.py
@@ -4,24 +4,20 @@ import numpy as np
 import tensorflow as tf
 from pathlib import Path
 from sklearn.model_selection import train_test_split
-from pykg2vec.utils.kgcontroller import KnowledgeGraph
+from pykg2vec.utils.kgcontroller import KnowledgeGraph, UserDefinedDataset
 from pykg2vec.config.config import KGEArgParser
-from model_config import transe_config, ntn_config
+from model_config import transe_config
 from pykg2vec.core.TransE import TransE
-from pykg2vec.core.NTN import NTN
 from pykg2vec.utils.trainer import Trainer
 
 
 class KnowledgeDataLoader:
 
-    def __init__(self, data_dir, test_size=0.2):
-        self._data_dir = Path(data_dir).resolve()
+    def __init__(self, data_dir, negative_sampling='uniform', test_size=0.2):
+        self._data_dir = Path(data_dir)
+        self._negative_sampling = negative_sampling
         self._triples: np.array = []
-        # this will change later
-        self._name = None if self._data_dir is None else self._data_dir.name.rsplit('/', 1)[-1].rsplit('.', 2)[0]
         self._test_size = test_size
-        self.load()
-        self.split()
 
     @property
     def data_dir(self):
@@ -32,13 +28,52 @@ class KnowledgeDataLoader:
         return self._triples
 
     @property
-    def name(self):
-        return self._name
+    def negative_sampling(self):
+        return self._negative_sampling
+
+    @property
+    def test_size(self):
+        return self._test_size
+
+    def get_knowledge_graph(self):
+        dataset_name = self.data_dir.stem if '.' not in self.data_dir.stem else self.data_dir.stem.split('.')[0]
+        custom_dataset_path = self.data_dir.parent / dataset_name
+        if not custom_dataset_path.exists():
+            Path(custom_dataset_path).mkdir(parents=True, exist_ok=True)
+
+        # Means directory is created, but split files don't exist
+        if not custom_dataset_path.is_file():
+            self.__load()
+            self.__split(write_path=custom_dataset_path, name=dataset_name)
+        return KnowledgeGraph(dataset=dataset_name, negative_sample=self.negative_sampling, custom_dataset_path=custom_dataset_path)
+
+    # TODO: Extend this functionality for parsing multiple filetypes + streaming for large files
+    # For loading large bz2 zipped n-triple files. Not extended for other files yet
+    # Can be extended for different file types - need smarter parser for very large files
+    def __load(self):
+        # Temporary catchall for bad files
+        acceptable_formats = ['.ttl', '.nt']
+        if self.data_dir.suffixes[0] in acceptable_formats:
+            triples = []
+            with bz2.BZ2File(self.data_dir, 'rb') as file:
+                for line in file:
+                    decoded_line = line.decode('utf-8').split()
+                    if decoded_line[0] == '#':
+                        continue
+                    triples.append((decoded_line[0], decoded_line[1], decoded_line[2]))
+            self.triples.extend(triples)
+        else:
+            raise Exception('File extension is not supported.')
+
+    def __split(self, write_path, name):
+        x_train, x_test = train_test_split(self.triples, test_size=self.test_size)
+        x_test, x_valid = train_test_split(x_test, test_size=0.5)
+        self.__write(self, triples=x_test, write_path=write_path / Path(name+'-test.txt'))
+        self.__write(self, triples=x_train, write_path=write_path / Path(name+'-train.txt'))
+        self.__write(self, triples=x_valid, write_path=write_path / Path(name+'-valid.txt'))
 
     @staticmethod
-    def write(self, triples: np.array, write_path: Path):
-        if not write_path.parent.exists():
-            Path(write_path.parent).mkdir(parents=True, exist_ok=True)
+    def __write(self, triples: np.array, write_path: Path):
         with open(write_path, 'w', encoding='utf-8') as f:
             for triple in triples:
                 try:
@@ -46,51 +81,29 @@ class KnowledgeDataLoader:
                 except UnicodeEncodeError:
                     continue
 
-    # For loading large n-triple files. Not extended for other files yet
-    # Can be extended for different file types - need smarter parser for very large files
-    def load(self):
-        if self._data_dir.suffixes[0] == '.ttl':
-            triples = []
-            with bz2.BZ2File(self._data_dir, 'rb') as file:
-                for line in file:
-                    decoded_line = line.decode('utf-8').split()
-                    if decoded_line[0] == '#':
-                        continue
-                    triples.append((decoded_line[0], decoded_line[1], decoded_line[2]))
-            self._triples.extend(triples)
-        else:
-            raise Exception('File extension is not supported.')
-
-    def split(self):
-        x_train, x_test = train_test_split(self._triples, test_size=self._test_size)
-        x_test, x_valid = train_test_split(x_test, test_size=0.5)
-        self.write(self, triples=x_test, write_path=Path('./data/'+self._name+'/'+self._name+'-test.txt'))
-        self.write(self, triples=x_train, write_path=Path('./data/'+self._name+'/'+self._name+'-train.txt'))
-        self.write(self, triples=x_valid, write_path=Path('./data/'+self._name+'/'+self._name+'-valid.txt'))
-
 
 def main():
 
     args = KGEArgParser().get_args(sys.argv[1:])
 
-    if args.dataset_path is None:
-        kg = KnowledgeGraph()
-        kg.prepare_data()
-        kg.dump()
+    if Path(args.dataset_path).exists():
+        kdl = KnowledgeDataLoader(data_dir=args.dataset_path, negative_sampling=args.sampling)
+        kg = kdl.get_knowledge_graph()
+        print('Successfully loaded {} triples from {}.'.format(len(kdl.triples), kdl.data_dir))
     else:
-        kgl = KnowledgeDataLoader(data_dir=args.dataset_path)
-        print('Successfully loaded {} triples from {}.'.format(len(kgl.triples), kgl.data_dir))
+        print('Unable to find dataset from path:', args.dataset_path)
+        print('Default loading Freebase15k dataset with default hyperparameters...')
+        kg = KnowledgeGraph()
 
-        args.dataset_path = './data/' + kgl.name
-        args.dataset_name = kgl.name
+    kg.prepare_data()
+    kg.dump()
 
-        # Define knowledge graph
-        kg = KnowledgeGraph(dataset=args.dataset_name, negative_sample=args.sampling,
-                            custom_dataset_path=args.dataset_path)
-        kg.prepare_data()
-        kg.dump()
+    # TODO: Not sure why new dataset isn't cached on subsequent hits...
+    args.dataset_path = './data/' + kg.dataset_name
+    args.dataset_name = kg.dataset_name
 
-    models = [TransE(transe_config(args=args)), NTN(ntn_config(args=args))]
+    # Add new model configurations to run.
+    models = [TransE(transe_config(args=args))]
 
     for model in models:
         print('---- Training Model: {} ----'.format(model.model_name))
@@ -100,8 +113,8 @@ def main():
         tf.reset_default_graph()
 
 
-# Custom Dataset with no tuning   :     python rdf_recommender.py -dsp "./data/topical_concepts_en.ttl.bz2" -plote True
-# Freebase15k with Bayesian tuning:     python rdf_recommender.py -ghp True
+# Custom Dataset with no tuning    :  python rdf_recommender.py -dsp "./data/topical_concepts_en.ttl.bz2" [-plote True]
+# Freebase15k with Bayesian tuning :  python rdf_recommender.py -ghp True [-plote True]
 if __name__ == "__main__":
     main()
 
